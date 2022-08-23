d97b3f45899796d645b45c50f00a4ca344dfcd54
akashkumar-404 <akashkumar404@gmail.com>
2022-08-23 23:00:11 +0530

    day-1

---
+++ my-app/src/pages/navbar.js
@@ -0,0 +1,196 @@
+
+
+function Navbar() {
+    return (  
+        <>
+        <div>
+        <div id="navbar">
+    <div className="navbar-left" id="navbar div">
+        <div><p href="./homepage.html">
+    <img src="https://wassets.hscicdn.com/static/images/logo.png"/></p>
+    </div>
+    <div>
+        <div className="dropdown">
+          <button className="dropbtn"><p href="../fp04_437/LiveScorcesHome.html" style={{color:'white',textDecoration:'none'}}>Live Scores</p> 
+            <i className="fa fa-caret-down"></i>
+          </button>
+          <div className="dropdown-content">
+            <p href="#">Live Scores Home</p>
+            <p href="#">Week view</p>
+            <p href="#">Month view</p>
+            <p href="#">Season view</p>
+            <p href="#">International calendar</p>
+            <p href="#">Desktop Scoreboard</p>
+          </div>
+        </div> 
+        <div className="dropdown">
+          <button className="dropbtn2">Series
+            <i className="fa fa-caret-down"></i>
+          </button>
+          <div className="dropdown-content2">
+            <div>
+            <p href="../fp04_437/series.html">West Indies v India</p>
+            <p href="#">England v South Africa</p>
+            <p href="#">Ireland v New Zealand</p>
+            <p href="#">Scotland v New Zealand</p>
+            <p href="#">SA-W in England</p>
+            <p href="#">County Div1</p>
+            <p href="#">Test Championship</p>
+            <p href="#">CWC Super League</p>
+            <p href="#">-Archives-</p>
+        </div>
+        <div>
+             <p href="#">TNPL 2022</p>
+            <p href="#">Sri Lanka v Pakistan</p>
+            <p href="#">Zimbabwe v Bangladesh</p>
+            <p href="#">IRE Tri-Series(Women)</p>
+            <p href="#">CWG Games(Cricket)</p>
+            <p href="#">County Div2 </p>
+            <p href="#">Women's Championship</p>
+            <p href="#">-Future series-</p>
+            
+        </div>
+      
+          </div>
+        </div>
+        <div className="dropdown" id="teamshref">
+            <button className="dropbtn1" ><p href="../fw19_0976/component/teamdin.html" style={{color:'white',textDecoration:'none'}}>Teams</p>
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content2">
+                <div>
+              <p href="#">Australia</p>
+              <p href="#">Bangladesh</p>
+              <p href="#">England</p>
+              <p hreF="../fw19_0976/component/india.html">India</p>
+              <p href="#">New Zealand</p>
+              <p href="#">Pakistan</p>
+              <p href="#">South Africa</p>
+              <p href="#">Sri Lanka</p>
+              <p href="#">West Indies</p>
+              <p href="#">Zimbabwe</p>
+            </div>
+            <div>
+                <p href="#">Afganistan</p>
+                <p href="#">Ireland</p>
+                <p href="#">Namibia</p>
+                <p href="#">Nepal</p>
+                <p href="#">Netherlands</p>
+                <p href="#">Oman</p>
+                <p href="#">PNG</p>
+                <p href="#">Scotland</p>
+                <p href="#">UAE</p>
+                <p href="#">USA</p>
+            </div>
+            </div>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn1"><p href="../newsnew.html" style={{color:'white',textDecoration:'none'}}>News</p>
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content1">
+              <p href="#">News Home</p>
+              <p href="#">Covid-19</p>
+              <p href="#">Ball-tampering</p>
+              <p href="#">Technology in cricket</p>
+              <p href="#">Racism</p>
+            </div>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn1"><p href="../fw18_0494/Featurs/index.html" style={{color:'white',textDecoration:'none'}}>Features</p> 
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content1">
+              <p href="#">Feature Home</p>
+              <p href="#">Writers</p>
+              <p href="#">Photo galleries</p>
+              <p href="#">The Cricket Monthly</p>
+            </div>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn2"><p href="../fw18_0494/Video/video.html" style={{color:'white',textDecoration:'none'}}>Videos</p> 
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content2">
+                <div>
+              <p href="../fw18_0494/news_detail/detail.html">Videos Home</p>
+              <p href="#">T20 Time Out Hindi</p>
+              <p href="#">Math Day</p>
+              <p>Fantasy pick</p>
+              <p href="#">Run Order</p>
+              <p href="#">25 Question</p>
+              <p href="#">Interviews</p>
+              <p href="#">Press Conference</p>
+            </div>
+            <div>
+                <p href="#">T20 Time Out</p>
+                <p href="#">Hindi Videos</p>
+                <p href="#">Haan Ya Naa</p>
+                <p href="#">Polite Enquiries</p>
+                <p href="#">Newsroom</p>
+                <p href="#">News and Analysis</p>
+                <p href="#">Features</p>
+                <p href="#">YouTube</p>
+            </div>
+            </div>
+          </div>
+          <div className="dropdown">
+            <button className="dropbtn2"><p href="../fw18_1217/index.html" style={{color:'white',textDecoration:'none'}}>Stats</p>
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content2">
+                <div>
+              <p href="#">Stats home</p>
+              <p href="#">IPL 2022</p>
+              <p href="#">SuperStats</p>
+              <p href="#">All records</p>
+              <p href="#">Grounds</p>
+            </div>
+            <div>
+                <p href="#">AskCrickinfo</p>
+                <p href="#">Statsguru</p>
+                <p href="#">2022 records</p>
+                <p href="#">Players</p>
+                <p href="#">Rankings</p>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div className="navbar-right">
+       <div className="fantasy" ><p href="../day 3/index.html"  style={{color:'white',textDecoration:'none'}}><img  src="https://wassets.hscicdn.com/static/images/fantasy-home.png"/><p>Fantasy</p></p></div>
+       <div className="dropdown">
+        <button className="dropbtn3">Edition IN
+        </button>
+        <div className="dropdown-content3">
+            <div>
+          <p href="#">Africa</p>
+          <p href="#">Bangladesh</p>
+          <p href="#">India-Hindi</p>
+          <p href="#">Pakistan</p>
+          <p href="#">United Kingdom</p>
+          <p href="#">Global</p>
+        </div>
+        <div>
+            <p href="#">Australia</p>
+            <p href="#">India</p>
+            <p href="#">New Zealand</p>
+            <p href="#">Sri Lanka</p>
+            <p href="#">United States</p>
+        </div>
+        </div>
+      </div>
+       <div id="night"><p><img src="../images/moon.JPG" />
+        </p></div>
+       <div><p><p>कA</p></p></div>
+       <div><p><img src="../images/grid.JPG" />
+        </p></div>
+       <div ><p><img src="../images/magnifying.JPG" /></p></div>
+    </div>
+</div>
+</div>
+</div>
+</>
+    )
+}
+
+export default Navbar;
